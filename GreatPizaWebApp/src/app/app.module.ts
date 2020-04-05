f27a27ad3baf393b22fa91376a5f9c4a03de88f8
CarlosCalderon <c_e1984@hotmail.com>
2020-04-05 14:08:16 -0400

    Components, Models, and Services Created

--- GreatPizaWebApp/src/app/app.module.ts
+++ GreatPizaWebApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PizzaComponent } from './pages/pizza/pizza.component';
+import { PizzasComponent } from './pages/pizza/pizzas.component';
+import { ToppingComponent } from './pages/topping/topping.component';
+import { ToppingsComponent } from './pages/topping/toppings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent,
+    PizzasComponent,
+    ToppingComponent,
+    ToppingsComponent
   ],
   imports: [
     BrowserModule,
