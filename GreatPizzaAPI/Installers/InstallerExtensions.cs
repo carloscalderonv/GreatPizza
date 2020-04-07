fef1cd48fc2db1c1c901a4574731ec2758b80922
Carlos Calderón <carloscalderonv@gmail.com>
2020-04-07 19:35:20 -0400

    Split lines for better visualization on InstallerExtensions.cs

--- GreatPizzaAPI/Installers/InstallerExtensions.cs
+++ GreatPizzaAPI/Installers/InstallerExtensions.cs
@@ -11,8 +11,8 @@ namespace GreatPizzaAPI
         public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
         {
             var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
-                typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
-
+                typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).
+                Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
             installers.ForEach(installer => installer.InstallServices(services, configuration));
         }
     }
