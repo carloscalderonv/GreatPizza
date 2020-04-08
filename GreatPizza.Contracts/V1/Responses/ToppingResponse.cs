1062cee4e4ea260e3fc995c1e8ae4c8ebc6bf3f9
Carlos Calderón <carloscalderonv@gmail.com>
2020-04-07 23:28:33 -0400

    Add Contracts Project Add References to AddProject From API

---
+++ GreatPizza.Contracts/V1/Responses/ToppingResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreatPizza.Contracts.V1.Responses
+{
+    class ToppingResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
