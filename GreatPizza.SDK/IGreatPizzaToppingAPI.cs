86d0b1ae9116638fa59aab89faca5329d694b303
Carlos Calderón <carloscalderonv@gmail.com>
2020-04-08 12:56:14 -0400

    Add SDK Project Create Interface for API Testing Pizza and Topping

---
+++ GreatPizza.SDK/IGreatPizzaToppingAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using GreatPizza.Contracts;
+using GreatPizza.Contracts.V1.Responses;
+using GreatPizza.Contracts.V1.Requests;
+namespace GreatPizza.SDK
+{
+    public interface IGreatPizzaToppingAPI
+    {
+        [Get("/api/v1/topping/")]
+        Task<ApiResponse<List<ToppingResponse>>>  GetAllToppingsAsync();
+
+        [Get("/api/v1/topping/{IdTopping}")]
+        Task<ApiResponse<ToppingResponse>> GetToppingAsync(Guid IdTopping);
+
+        [Post("/api/v1/topping/")]
+        Task<ApiResponse<ToppingResponse>> CreateToppingAsync([Body] CreateToppingRequest createToppingRequest);
+
+        [Put("/api/v1/topping/{IdTopping}")]
+        Task<ApiResponse<ToppingResponse>> UpdateToppingAsync(Guid IdTopping, [Body] UpdateToppingRequest updateToppingRequest);
+
+        [Delete("/api/v1/topping/{IdTopping}")]
+        Task<ApiResponse<string>> DeleteToppingAsync(Guid IdTopping);
+    }
+}
